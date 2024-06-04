300fd520412f0bc1af3c28cd11076ca1b405095c
Meisie Widuri <ewprjkt@proton.me>
2024-06-04 20:14:58 +0700

    test

--- build.sh
+++ build.sh
@@ -24,32 +24,48 @@ cdir() {
 
 # Main
 MainPath="$(pwd)"
-MainClangPath="${MainPath}/clang"
-ClangPath="${MainClangPath}"
+ClangPath="${MainPath}/clang"
+GCCaPath="${MainPath}/GCC64"
+GCCbPath="${MainPath}/GCC32"
 
 # Identity
 VERSION=14
 KERNELNAME=TheOneMemory
-CODENAME=Hayzel
-VARIANT=HMP
+CODENAME=EOL
+VARIANT=EAS
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Clone Kernel Source
-git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm636 -b 14 kernel
+git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-eas-wip kernel
 
 # Clone StRess Clang
 ClangPath=${MainClangPath}
 [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
-mkdir $ClangPath
 rm -rf $ClangPath/*
+mkdir -p $ClangPath
 msg "|| Cloning StRess clang 16 ||"
-git clone --depth=1 https://gitlab.com/strongreasons/stress-clang.git $ClangPath
+#git clone --depth=1 https://gitlab.com/strongreasons/stress-clang.git $ClangPath
+msg "|| Cloning AOSP clang 18 ||"
+wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r510928.tar.gz -O "clang-r510928.tar.gz"
+tar -xf clang-r510928.tar.gz -C $ClangPath
+
+# Clone GCC
+rm -rf $GCCaPath/*
+rm -rf $GCCbPath/*
+mkdir -p $GCCaPath
+mkdir -p $GCCbPath
+msg "|| Cloning AOSP GCC 4.9.x ||"
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc64.tar.gz"
+tar -xf gcc64.tar.gz -C $GCCaPath
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc32.tar.gz"
+tar -xf gcc32.tar.gz -C $GCCbPath
 
 # Prepared
 KERNEL_ROOTDIR=$(pwd)/kernel # IMPORTANT ! Fill with your kernel source root directory.
-export KBUILD_BUILD_USER=queen # Change with your own name or else.
+export KBUILD_BUILD_USER=Queen # Change with your own name or else.
+export KBUILD_BUILD_HOST=UpsideDownCake # Change with your own host name or else.
 IMAGE=$(pwd)/kernel/out/arch/arm64/boot/Image.gz-dtb
 CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
@@ -79,20 +95,38 @@ export COMMIT_HEAD=$(git log --oneline -1)
 make -j$(nproc) O=out ARCH=arm64 X00TD_defconfig
 make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
     LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}" \
-    PATH=$ClangPath/bin:${PATH} \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    CC=clang \
-    AR=llvm-ar \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip \
-    NM=llvm-nm \
-    OBJCOPY=llvm-objcopy \
-    READELF=llvm-readelf \
-    HOSTAR=llvm-ar \
-    HOSTAS=llvm-as \
-    HOSTLD=ld.lld \
-    LD="ld.lld"
+    PATH=$ClangPath/bin:$GCCaPath/bin:$GCCbPath/bin:/usr/bin:${PATH} \
+    CC=${ClangPath}/bin/clang \
+    NM=${ClangPath}/bin/llvm-nm \
+    CXX=${ClangPath}/bin/clang++ \
+    AR=${ClangPath}/bin/llvm-ar \
+    STRIP=${ClangPath}/bin/llvm-strip \
+    OBJCOPY=${ClangPath}/bin/llvm-objcopy \
+    OBJDUMP=${ClangPath}/bin/llvm-objdump \
+    OBJSIZE=${ClangPath}/bin/llvm-size \
+    READELF=${ClangPath}/bin/llvm-readelf \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    HOSTAR=${ClangPath}/bin/llvm-ar \
+    HOSTCC=${ClangPath}/bin/clang \
+    HOSTCXX=${ClangPath}/bin/clang++
+
+#    LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}" \
+#    PATH=$ClangPath/bin:${PATH} \
+#    CROSS_COMPILE=aarch64-linux-gnu- \
+#    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+#    CC=clang \
+#    AR=llvm-ar \
+#    OBJDUMP=llvm-objdump \
+#    STRIP=llvm-strip \
+#    NM=llvm-nm \
+#    OBJCOPY=llvm-objcopy \
+#    READELF=llvm-readelf \
+#    HOSTAR=llvm-ar \
+#    HOSTAS=llvm-as \
+#    HOSTLD=ld.lld \
+#    LD="ld.lld"
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
@@ -100,9 +134,10 @@ make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
    fi
 
    msg "|| Cloning AnyKernel ||"
-   git clone --depth=1 https://github.com/strongreasons/AnyKernel3 -b hmp-12 AnyKernel
+   git clone --depth=1 https://github.com/Tiktodz/AnyKernel3 -b 44 AnyKernel
 	cp $IMAGE AnyKernel
 }
+
 # Push kernel to telegram
 function push() {
     cd AnyKernel
